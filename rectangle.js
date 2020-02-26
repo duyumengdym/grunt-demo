a2f70924734340924338128564d2e47523382e75
duyumengdym <272780382@qq.com>
2020-02-26 12:02:35 +0800

    eslint

--- rectangle.js
+++ rectangle.js
@@ -1,28 +1,19 @@
+/* global $:true*/ 
 $(function() {
-    // get dom elem
-    var $width = $('#width'),
-        $height = $('#height'),
-        $btnCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
-    /**解决浮点舍入误差 */
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
-    /*calc button click event */
-    $btnCal.click(function(){
-      // get value
-      var w = Number($width.val()),
-          h = Number($height.val());
-      // calculate
-      var p = 2 * roundFractional(w + h, 1),
-          a = roundFractional(w * h, 2);
-          ;   
-      // output 
-      $perimeter.val(p);
-      $area.val(a);
-    });
-    
-    
-      
-  });
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  function roundFractional(x, n) {
+    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  }
+  $btnCal.click(function(){
+    var w = Number($width.val()),
+        h = Number($height.val());
+    var p = 2 * roundFractional(w + h, 1),
+        a = roundFractional(w * h, 2);
+    $perimeter.val(p);
+    $area.val(a);
+  }); 
+});
